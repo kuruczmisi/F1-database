1d7531eb3fa49f92877e6ce551e6ca2745224857
Kurucz Mihály <kurucz.misi@icloud.com>
2026-07-02 19:48:52 +0200

    Initial commit

---
+++ src/main/java/org/example/f1database/F1DatabaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.f1database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1DatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F1DatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/f1database/F1DatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.f1database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1DatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
